d3e9093ae62471abc6eca5498e95aa64b3e0f29e
Hassan Amjad <hassanamjad9529@gmail.com>
2026-04-10 06:34:18 +0500

    warp perspective

--- chapter_1.py
+++ chapter_1.py
@@ -1,14 +1,16 @@
 import cv2
 import numpy as np
-
 img = cv2.imread("Resources/whatsapp.jpeg")
-print(img.shape)
-imgResize = cv2.resize(img, (300,900))
-print(imgResize.shape)
 
-imgCropped = img[0:200]
+width, height= 250,350
+
+pts1 = np.float32([[111, 219],[287, 188],[154, 482],[352, 440]])
+pts2 = np.float32([[0,0],[width, 0],[0, height],[width, height]])
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+imtOutput = cv2.warpPerspective(img, matrix, (width, height))
 
 
-cv2.imshow("Original Image", img)
-cv2.imshow("Resized Image", imgResize)
+cv2.imshow("image", img)
+cv2.imshow("Output", imtOutput)
+
 cv2.waitKey(0)
