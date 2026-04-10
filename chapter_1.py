0d1166a4e251e48d9cd1fab70d7955e25b5d0478
Hassan Amjad <hassanamjad9529@gmail.com>
2026-04-10 06:39:51 +0500

    image stack

--- chapter_1.py
+++ chapter_1.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 img = cv2.imread("Resources/whatsapp.jpeg")
 
-width, height= 250,350
-
-pts1 = np.float32([[111, 219],[287, 188],[154, 482],[352, 440]])
-pts2 = np.float32([[0,0],[width, 0],[0, height],[width, height]])
-matrix = cv2.getPerspectiveTransform(pts1, pts2)
-imtOutput = cv2.warpPerspective(img, matrix, (width, height))
 
 
-cv2.imshow("image", img)
-cv2.imshow("Output", imtOutput)
+
+# imgHor = np.hstack((img, img))
+# imgVer = np.vstack((img, img))
+#
+# cv2.imshow("horizontal", imgHor)
+# cv2.imshow("vertical", imgVer)
+#
+#
 
 cv2.waitKey(0)
